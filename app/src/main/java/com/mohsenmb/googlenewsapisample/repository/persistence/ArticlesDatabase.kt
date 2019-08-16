72215923fd38a1375492a10ad0213b5b4a305cad
Mohsen Bahman Poor <mohsenmbcom@gmail.com>
2019-08-16 03:37:20 +0300

    Disabled database schemaExport because of silly warnings

--- app/src/main/java/com/mohsenmb/googlenewsapisample/repository/persistence/ArticlesDatabase.kt
+++ app/src/main/java/com/mohsenmb/googlenewsapisample/repository/persistence/ArticlesDatabase.kt
@@ -25,7 +25,7 @@ object DateTypeConverter {
  *      https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/index.html?index=..%2F..index#6
  */
 @TypeConverters(DateTypeConverter::class)
-@Database(entities = [PersistedArticle::class], version = 1)
+@Database(entities = [PersistedArticle::class], version = 1, exportSchema = false)
 abstract class ArticlesDatabase : RoomDatabase() {
 
     companion object {

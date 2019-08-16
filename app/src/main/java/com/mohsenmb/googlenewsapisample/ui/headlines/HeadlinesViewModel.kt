33599db97bfb3cc5ce52e69669c87a91b5af6b89
Mohsen Bahman Poor <mohsenmbcom@gmail.com>
2019-08-16 16:45:39 +0300

    Small changes and improvements on the view model and change on the code style (space to tab)

--- app/src/main/java/com/mohsenmb/googlenewsapisample/ui/headlines/HeadlinesViewModel.kt
+++ app/src/main/java/com/mohsenmb/googlenewsapisample/ui/headlines/HeadlinesViewModel.kt
@@ -22,115 +22,117 @@ private const val PAGE_SIZE = 21
  *      https://developer.android.com/topic/libraries/architecture/viewmodel
  */
 class HeadlinesViewModel @Inject constructor(
-    val newsApi: NewsApi,
-    val dao: ArticlesDao,
-    val gson: Gson
+	val newsApi: NewsApi,
+	val dao: ArticlesDao,
+	val gson: Gson
 ) : ViewModel() {
 
-    private var fromDate = Date()
-    private var page = 1
-    private var goOffline = false
+	private var fromDate = Date()
+	private var page = 1
+	private var goOffline = false
 
-    private val articles: MutableLiveData<List<PersistedArticle>> by lazy { MutableLiveData<List<PersistedArticle>>() }
-    private val errors: MutableLiveData<Error> by lazy { MutableLiveData<Error>() }
+	private val articles: MutableLiveData<List<PersistedArticle>> by lazy { MutableLiveData<List<PersistedArticle>>() }
+	private val errors: MutableLiveData<Error> by lazy { MutableLiveData<Error>() }
 
-    private val disposables = CompositeDisposable()
-
-
-    fun getArticlesLiveData() = articles
-    fun getErrorsLiveData() = errors
-
-    fun firstPage() {
-        page = 1
-        fromDate = Date()
-        loadArticles(false)
-    }
-
-    fun nextPage() {
-        loadArticles(true)
-    }
+	private val disposables = CompositeDisposable()
 
 
-    private fun loadArticles(append: Boolean) {
-        if (goOffline) {
-            dao.fetchNews(fromDate, PAGE_SIZE)
-                .map { articles ->
-                    if (articles.isEmpty()) {
-                        throw UnsupportedOperationException(NO_MORE_DATA)
-                    }
-                    return@map articles
-                }
-                .observe(true, append)
-        } else {
-            newsApi.loadTopHeadlines(page, PAGE_SIZE)
-                .map { response ->
-                    response.articles.let { responseArticles ->
-                        page++
-                        if (responseArticles.size < PAGE_SIZE) {
-                            goOffline = true
-                        }
-                        if (responseArticles.isEmpty()) {
-                            throw UnsupportedOperationException(NO_MORE_ONLINE)
-                        }
-                        responseArticles.map { article ->
-                            article.toPersistedArticle()
-                        }
-                    }
-                }.observe(false, append)
-        }
+	fun getArticlesLiveData() = articles
+	fun getErrorsLiveData() = errors
 
-    }
+	fun firstPage() {
+		page = 1
+		fromDate = Date()
+		loadArticles(false)
+	}
 
-    override fun onCleared() {
-        disposables.clear()
-        super.onCleared()
-    }
+	fun nextPage() {
+		loadArticles(true)
+	}
 
-    private fun Single<List<PersistedArticle>>.observe(offlineDate: Boolean, append: Boolean) {
-        disposables.add(
-            subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ response ->
-                    errors.value = noError()
 
-                    fun publishData() {
-                        val outArticles = when (append) {
-                            true -> {
-                                val existing = articles.value ?: emptyList()
-                                (existing + response)
-                            }
-                            false -> {
-                                response
-                            }
-                        }.apply { sortedByDescending { it.publishDate } }
-                        articles.value = outArticles
-                        fromDate = outArticles.last().publishDate
-                    }
+	private fun loadArticles(append: Boolean) {
+		if (goOffline) {
+			dao.fetchNews(fromDate, PAGE_SIZE)
+				.map { articles ->
+					if (articles.isEmpty()) {
+						throw UnsupportedOperationException(NO_MORE_DATA)
+					}
+					return@map articles
+				}
+				.observe(true, append)
+		} else {
+			newsApi.loadTopHeadlines(page, PAGE_SIZE)
+				.map { response ->
+					response.articles.let { responseArticles ->
+						page++
+						if (responseArticles.size < PAGE_SIZE) {
+							goOffline = true
+						}
+						if (responseArticles.isEmpty()) {
+							throw UnsupportedOperationException(NO_MORE_ONLINE)
+						}
+						responseArticles.map { article ->
+							article.toPersistedArticle()
+						}
+					}
+				}.observe(false, append)
+		}
 
-                    if (!offlineDate) {
-                        dao.insertAll(response).observe {
-                            publishData()
-                        }
-                    } else {
-                        publishData()
-                    }
-                }, parseError(gson) { error ->
-                    errors.value = error
-                    if (error.type == ErrorType.NetworkOffline ||
-                        error.type == ErrorType.NoMoreOnlineData
-                    ) {
-                        goOffline = true
-                        loadArticles(append)
-                    }
-                })
-        )
-    }
+	}
 
-    private fun Completable.observe(onComplete: () -> Unit = {}) {
-        disposables.add(
-            subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(onComplete)
-        )
-    }
+	override fun onCleared() {
+		disposables.clear()
+		super.onCleared()
+	}
+
+	private fun Single<List<PersistedArticle>>.observe(offlineData: Boolean, append: Boolean) {
+		disposables.add(
+			map { rawResponse ->
+				val outArticles = when (append) {
+					true -> {
+						val existing = articles.value ?: emptyList()
+						(existing + rawResponse)
+					}
+					false -> {
+						rawResponse
+					}
+				}.apply { sortedByDescending { it.publishDate } }
+				outArticles
+			}.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe({ response ->
+					errors.value = noError()
+
+					fun publishData() {
+						fromDate = response.last().publishDate
+						articles.value = response
+					}
+
+					if (!offlineData) {
+						dao.insertAll(response).observe {
+							publishData()
+						}
+					} else {
+						publishData()
+					}
+				}, parseError(gson) { error ->
+					errors.value = error
+					if (error.type == ErrorType.NetworkOffline ||
+						error.type == ErrorType.NoMoreOnlineData
+					) {
+						goOffline = true
+						loadArticles(append)
+					}
+				})
+		)
+	}
+
+	private fun Completable.observe(onComplete: () -> Unit = {}) {
+		disposables.add(
+			subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(onComplete)
+		)
+	}
 }

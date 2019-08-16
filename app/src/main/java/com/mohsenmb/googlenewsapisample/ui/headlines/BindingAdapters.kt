bd81d9ce0e0ffce7c859ae916f5c1399784c0e7d
Mohsen Bahman Poor <mohsenmbcom@gmail.com>
2019-08-16 16:47:28 +0300

    Added binding adapter of the news list with some styling tweaks

---
+++ app/src/main/java/com/mohsenmb/googlenewsapisample/ui/headlines/BindingAdapters.kt
@@ -0,0 +1,36 @@
+package com.mohsenmb.googlenewsapisample.ui.headlines
+
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.databinding.BindingAdapter
+import com.github.marlonlom.utilities.timeago.TimeAgo
+import com.mohsenmb.googlenewsapisample.R
+import com.squareup.picasso.Picasso
+import java.util.*
+
+object BindingAdapters {
+
+	@JvmStatic
+	@BindingAdapter("imageUrl")
+	fun setImageUrl(imageView: AppCompatImageView, url: String?) {
+		if (url == null) {
+			imageView.setImageResource(R.drawable.img_news_placeholder)
+		} else {
+			Picasso.get()
+				.load(url)
+				.placeholder(R.drawable.img_news_placeholder)
+				.error(R.drawable.img_news_placeholder)
+				.resize(250, 250)
+				.centerCrop()
+				.into(imageView)
+		}
+	}
+
+	@JvmStatic
+	@BindingAdapter("time")
+	fun setImageUrl(textView: AppCompatTextView, date: Date?) {
+		if (date != null) {
+			textView.text = TimeAgo.using(date.time)
+		}
+	}
+}
---
+++ app/src/main/java/com/mohsenmb/googlenewsapisample/ui/headlines/NewsListAdapter.kt
@@ -0,0 +1,113 @@
+package com.mohsenmb.googlenewsapisample.ui.headlines
+
+import android.graphics.Rect
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.mohsenmb.googlenewsapisample.BR
+import com.mohsenmb.googlenewsapisample.R
+import com.mohsenmb.googlenewsapisample.repository.persistence.PersistedArticle
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * I had written binding adapter before, checkout the following repo on my account
+ * 		https://github.com/mohsenmbcom/GithubApiTest
+ */
+class DataBindingViewHolder(private val binding: ViewDataBinding) :
+	RecyclerView.ViewHolder(binding.root) {
+
+	fun bindVariable(variableId: Int, obj: Any) {
+		binding.setVariable(variableId, obj)
+		binding.executePendingBindings()
+	}
+}
+
+class NewsRecyclerAdapter() : RecyclerView.Adapter<DataBindingViewHolder>() {
+
+	private val articles = mutableListOf<PersistedArticle>()
+	private val disposables = CompositeDisposable()
+	var onItemClickListener : (PersistedArticle) -> Unit = { }
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+			DataBindingViewHolder {
+		val layoutInflater = LayoutInflater.from(parent.context)
+		val binding: ViewDataBinding =
+			DataBindingUtil.inflate(layoutInflater, R.layout.cell_article, parent, false)
+		return DataBindingViewHolder(binding)
+	}
+
+	override fun getItemCount(): Int = articles.size
+
+	override fun onBindViewHolder(holder: DataBindingViewHolder, position: Int) {
+		holder.bindVariable(BR.article, articles[position])
+		holder.bindVariable(BR.onItemClickListener, onItemClickListener)
+	}
+
+	fun updateNews(news: List<PersistedArticle>) {
+		Log.e("ListSize", "${news.size}")
+		disposables.clear()
+		disposables.add(
+			Single.just(articles to news)
+				.map { (oldList, newList) ->
+					val diff = DiffUtil.calculateDiff(NewsDiffCallback(oldList, newList), false)
+					oldList.apply {
+						clear()
+						addAll(newList)
+					}
+					diff
+				}
+				.subscribeOn(Schedulers.computation())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe({ diff ->
+					diff.dispatchUpdatesTo(this@NewsRecyclerAdapter)
+				}, {
+					it.printStackTrace()
+				})
+		)
+	}
+
+}
+
+class NewsDiffCallback(val oldList: List<PersistedArticle>, val newList: List<PersistedArticle>) :
+	DiffUtil.Callback() {
+	override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+		oldList[oldItemPosition] == newList[newItemPosition]
+
+	override fun getOldListSize(): Int = oldList.size
+
+	override fun getNewListSize(): Int = newList.size
+
+	override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+		oldList[oldItemPosition] == newList[newItemPosition]
+
+}
+
+/**
+ *  https://stackoverflow.com/questions/28531996/android-recyclerview-gridlayoutmanager-column-spacing
+ */
+class SpacesItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+
+	override fun getItemOffsets(
+		outRect: Rect, view: View,
+		parent: RecyclerView, state: RecyclerView.State
+	) {
+		outRect.left = space
+		outRect.right = space
+		outRect.bottom = space
+
+		// Add top margin only for the first item to avoid double space between items
+		if (parent.getChildLayoutPosition(view) == 0) {
+			outRect.top = space
+		} else {
+			outRect.top = 0
+		}
+	}
+}
